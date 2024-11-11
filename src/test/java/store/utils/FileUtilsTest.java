9ba67636da18e4a13d8f9dda26f9c0e640653e7a
yesjuhee <juhee0924@g.skku.edu>
2024-11-11 23:57:53 +0900

    test: 테스트 추가

--- src/test/java/store/utils/FileUtilsTest.java
+++ src/test/java/store/utils/FileUtilsTest.java
@@ -9,7 +9,7 @@ import store.constant.Path;
 
 class FileUtilsTest {
     @Test
-    void 원하는파일을읽어서_2차원배열로반환한다() {
+    void 프로덕트_파일을읽어서_2차원배열로반환한다() {
         // given
         String filePath = Path.PATH_TO_RESOURCES + "products.md";
 
@@ -20,5 +20,19 @@ class FileUtilsTest {
         assertThat(data)
                 .hasSize(16)
                 .contains(List.of("컵라면", "1700", "10", "null"), Index.atIndex(15));
+    }
+
+    @Test
+    void 프로모션_파일을읽어서_2차원배열로반환한다() {
+        // given
+        String filePath = Path.PATH_TO_RESOURCES + "promotions.md";
+
+        // when
+        List<List<String>> data = FileUtils.readCsvBody(filePath);
+
+        // then
+        assertThat(data)
+                .hasSize(5)
+                .contains(List.of("promotion1", "2", "1", "2024-01-01", "2024-12-31"), Index.atIndex(0));
     }
 }
